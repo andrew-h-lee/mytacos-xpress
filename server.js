eeb20591cd695f9f68bcfbcc5d210b55a42a5b2f
andrew-h-lee <mr.ahlee@gmail.com>
2016-06-23 00:01:30 +1000

    Add New Changes

--- server.js
+++ server.js
@@ -12,10 +12,11 @@ app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
 
-
-
-
 /* Test out if app is run directly or relies another file */
-if(!module.parent) {
-	
+if (!module.parent) {
+	app.listen(app.get('port'), () => {
+    console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+  });
 }
+
+module.exports = app;
