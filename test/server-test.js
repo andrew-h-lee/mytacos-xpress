656b7da9e57135eecaa5598f0844acb1119d8662
andrew-h-lee <mr.ahlee@gmail.com>
2016-06-28 00:34:12 +1000

    Update Test Changes

--- test/server-test.js
+++ test/server-test.js
@@ -1,10 +1,31 @@
 const assert = require('assert');
-const app = require('./server');
+const app = require('../server');
+const request = require('request');
 
-desrcibe('Server', () => {
+describe('Server', () => {
+
+  before(done => {
+    this.port = 9876;
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
 
   it('should exist', () => {
     assert(app);
+  });
+
+  describe('GET /', () => {
+
+    it('should return a 200', (done) => {
+      
+    });
+
   });
 
 });
